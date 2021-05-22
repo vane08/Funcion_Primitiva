593cd0a3eb07e295800535274cb75fad44d094b7
vane08 <82453121+vane08@users.noreply.github.com>
2021-05-21 23:39:23 -0500

    Add files via upload

---
+++ FuncionPrimitiva17VanessaCruzHuitron3202.m
@@ -0,0 +1,13 @@
+%Titulo:Funcion Primitiva (17)
+%Nombre:Vanessa Cruz Huitrón 
+%Grupo:3202
+%Fecha: Viernes 21 de Mayo del 2021
+
+
+pkg load symbolic
+clear     
+clc
+syms u
+int (5*u^(3/2))
+ 
+ 
